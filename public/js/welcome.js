ba6aa6f308f54887dc4863e6832e34201074dbf8
vincent-kartamulya <vincent.learn01@gmail.com>
2023-03-31 02:44:59 +0700

    First commit

---
+++ public/js/welcome.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-input",{
+    strings: ["Vincent", "Student", "Human", "Sneakys"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true
+})
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom-color">
+    <div class="container text-white">
+    <a class="navbar-brand brand-custom" href="#">All About Vincent</a>
+        <button class="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse collapse-custom" id="navbarNav">
+            <ul class="navbar-nav ms-auto" >
+            <li class="nav-item p">
+                <a class="nav-link" href="#">General</a>
+            </li>
+            <li class="nav-item p">
+                <a class="nav-link" href="#">Learning</a>
+            </li>
+            <li class="nav-item p">
+                <a class="nav-link" href="#">Project</a>
+            </li>
+            <li class="nav-item p">
+                <a class="nav-link" href="#">Contact Me</a>
+            </li>
+            <li class="nav-item p">
+                <a class="nav-link" href="#">Take a Photo</a>
+            </li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+
+    <div class="header">
+        <div class="text-header">
+            <h1>Hi! I'm <span class="auto-input"></span></h1>
+            <p>But my friends also know me as Chincent(?). Always enthusiast in learning and trying new things, because as Chitato's motto goes, Life is never flat!</p>
+        </div>
+        <div class="image-header">
+            <img src="img/test.jpg" alt="">
+        </div>
+    </div>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/typed.js@2.0.14/dist/typed.umd.js"></script>
+    <script src="js/welcome.js"></script>
+</body>
+</html>
